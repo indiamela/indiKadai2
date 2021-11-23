f50e7f9a8228e3997a2895cb7f9718a90a765960
akio0911 <akio0911@gmail.com>
2021-11-23 09:54:04 +0900

    テスト修正

--- indiKadai-2/ViewController.swift
+++ indiKadai-2/ViewController.swift
@@ -16,11 +16,11 @@ final class ViewController: UIViewController {
     
     @IBAction private func didTapCalcButton(_ sender: Any){
         let num = calcSegmentControll.selectedSegmentIndex
-        let calcOption = Option(rawValue: num) ?? .plus
+        let calcOption = CalculationOption(rawValue: num) ?? .plus
         resultLabel.text = calculate(option: calcOption)
     }
     
-    private func calculate(option: Option) -> String? {
+    private func calculate(option: CalculationOption) -> String? {
         guard let num1 = textField1.text.flatMap({ Double($0) }), let num2 = textField2.text.flatMap({ Double($0) }) else {
             return "エラー：数値を入力してください"
         }
@@ -28,7 +28,7 @@ final class ViewController: UIViewController {
         do {
             let result = try option.calclate(with: num1, num2)
             return String(result)
-        } catch Option.Error.divisorIsZero {
+        } catch CalculationOption.Error.divisorIsZero {
             return "割る数には0以外を入力してください"
         } catch {
             return nil
@@ -36,31 +36,29 @@ final class ViewController: UIViewController {
     }
 }
 
-extension ViewController {
-    private enum Option: Int {
-        enum Error: Swift.Error {
-            case divisorIsZero
-        }
-        
-        case plus
-        case minus
-        case multiple
-        case division
-        
-        func calclate(with num1: Double, _ num2: Double) throws -> Double {
-            switch self {
-            case .plus:
-                return num1 + num2
-            case .minus:
-                return num1 - num2
-            case .multiple:
-                return num1 * num2
-            case .division:
-                guard num2 != 0 else {
-                    throw Error.divisorIsZero
-                }
-                return num1 / num2
+enum CalculationOption: Int {
+    enum Error: Swift.Error {
+        case divisorIsZero
+    }
+    
+    case plus
+    case minus
+    case multiple
+    case division
+    
+    func calclate(with num1: Double, _ num2: Double) throws -> Double {
+        switch self {
+        case .plus:
+            return num1 + num2
+        case .minus:
+            return num1 - num2
+        case .multiple:
+            return num1 * num2
+        case .division:
+            guard num2 != 0 else {
+                throw Error.divisorIsZero
             }
+            return num1 / num2
         }
     }
 }
